c0eef3b9dd0f1181410f35f096ac3095137575da
Yousef Nafaa <nafaa.youssef93@gmail.com>
2020-09-23 15:41:47 +0200

    commmit of first release of backend app

---
+++ src/main/java/com/project/ecommerceBackEndApp/EcommerceBackEndAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.ecommerceBackEndApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackEndAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackEndAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ecommerceBackEndApp/EcommerceBackEndAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ecommerceBackEndApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackEndAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
